ebac6c3b72716e554530b108e3e0ffdfdf63aebf
fuuuu <sanchan.fuu@gmail.com>
2021-05-04 15:50:46 +0900

    カテゴリーテーブルの更新、機能追加

--- laravel/app/Http/Controllers/SellController.php
+++ laravel/app/Http/Controllers/SellController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ItemCondition;
+use App\Models\PrimaryCategory;
 
 class SellController extends Controller
 {
@@ -12,9 +13,20 @@ class SellController extends Controller
         //$conditionsにitem_conditionsテーブルのクエリ結果を格納している
             // get()によりクエリを発行
             // orderby('引数1(カラムを指定)', '引数2(asc or desc)')によりクエリ結果のソートをする 未入力はasc
-        $conditions =ItemCondition::orderby('sort_no')->get();
+        $conditions = ItemCondition::orderBy('sort_no')->get();
+
+        // N+1問題対処のために with()を利用してEager Loadingを実施
+        $categories = PrimaryCategory::query()
+            ->with([
+                'secondaryCategories' => function($query){
+                    $query->orderBy('sort_no');
+                }
+            ])
+            ->orderBy('sort_no')
+            ->get();
 
         return view('sell')
-            ->with('conditions', $conditions);
+            ->with('conditions', $conditions)
+            ->with('categories', $categories);
     }
 }
--- laravel/app/Models/PrimaryCategory.php
+++ laravel/app/Models/PrimaryCategory.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class PrimaryCategory extends Model
 {
-    //
+    public function secondaryCategories()
+    {
+        // 1対多のリレーションを定義する
+        return $this->hasMany(SecondaryCategory::class);
+    }
 }
